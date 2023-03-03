b0a63f23878c6aaef6025b682737dbceeff0c988
MistreanuEmanuela <100144278+MistreanuEmanuela@users.noreply.github.com>
2023-03-03 18:45:39 +0200

    Create location.java

---
+++ Lb2/compulsory/location.java
@@ -0,0 +1,60 @@
+enum LocationType{
+    ORAS,
+    AEROPORT,
+    BENZINARIE;
+}
+
+public class location {
+        private String name;
+        private LocationType type;
+        private float x, y;
+
+    public location(String name, LocationType type, float x, float y) {
+        this.name = name;
+        this.type = type;
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setType(LocationType type) {
+            this.type = type;
+        }
+
+        public void setX(float x) {
+            this.x = x;
+        }
+
+        public void setY(float y) {
+            this.y = y;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public LocationType getType() {
+            return type;
+        }
+
+        public float getX() {
+            return x;
+        }
+
+        public float getY() {
+            return y;
+        }
+
+        @Override
+        public String toString() {
+            return "location{" +
+                    "name='" + name + '\'' +
+                    ", type=" + type +
+                    ", x=" + x +
+                    ", y=" + y +
+                    '}';
+        }
+    }
