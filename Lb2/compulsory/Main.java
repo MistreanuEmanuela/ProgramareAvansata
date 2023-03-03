8f2e75cf99eea8b69d62741c491180a8b690f14e
MistreanuEmanuela <100144278+MistreanuEmanuela@users.noreply.github.com>
2023-03-03 19:07:31 +0200

    Update Main.java

--- Lb2/compulsory/Main.java
+++ Lb2/compulsory/Main.java
@@ -4,17 +4,17 @@ public class Main {
         return Math.sqrt((x2-x1)*(x2-x1)-(y2-y1)*(y2-y1));
     }
     public static void main(String[] args) {
-        location a=new location("Iasi",LocationType.ORAS,40, 20);
+        Location a=new Location("Iasi",LocationType.ORAS,40, 20);
         a.setType(LocationType.ORAS);
         a.setX(40);
         a.setY(20);
         System.out.println(a.toString());
-        location b=new location("Botosani", LocationType.ORAS, 60,20);
+        Location b=new Location("Botosani", LocationType.ORAS, 60,20);
         b.setType(LocationType.ORAS);
         b.setY(20);
         b.setX(60);
         System.out.println(b.toString());
-        road A1=new road(100,RoadType.AUTOSTRADA, 90, a,b);
+        Road A1=new Road(100,RoadType.AUTOSTRADA, 90, a,b);
         A1.setLimit(90);
         A1.setType(RoadType.AUTOSTRADA);
         A1.setLenght(120);
